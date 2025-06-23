13df5db8aab52db34f8d0b39bf1fc9494dc32047
Elvira Darisheva <d.elvira@rocketfirm.com>
2025-06-23 11:31:26 +0500

    fix: lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync'
 import greeting from './cli.js'
 
-const roundsCount = 3;
+const roundsCount = 3
 
 export default function gameEngine(description, generateRound) {
   const name = greeting()
@@ -14,9 +14,7 @@ export default function gameEngine(description, generateRound) {
     const userAnswer = readlineSync.question('Your answer: ')
 
     if (userAnswer !== correctAnswer) {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      )
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
       console.log(`Let's try again, ${name}!`)
       return
     }
